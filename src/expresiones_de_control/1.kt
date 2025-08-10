839c179746fb4c0e814d85c188dc126361098183
YhermanIriza <yhermanyj@gmail.com>
2025-08-10 15:15:31 -0500

    6

---
+++ src/expresiones_de_control/1.kt
@@ -0,0 +1,9 @@
+fun main() {
+    for (posicion in 1..10) {
+        println("Posición $posicion")
+        if (posicion == 6) {
+            println("¡Salida encontrada!")
+            break
+        }
+    }
+}
